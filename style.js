958cd6aa2bae4c7da9c543f22d5e6d0ab794837e
Dipesh Devkota <131356364+DipeshDevkota@users.noreply.github.com>
2024-05-15 22:23:23 +0545

    Add files via upload

---
+++ style.js
@@ -0,0 +1,25 @@
+//js multiple text//
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const typed = new Typed('.multiple-text', {
+            strings: ['Frontend Developer'],
+            typeSpeed: 100,
+            backSpeed: 100,
+            backDelay: 1000,
+            loop: true
+        });
+    });
+
+
+    // window.addEventListener("scroll", function() {
+    //     var navbar = document.querySelector(".logo");
+    //     console.log(navbar);
+    //     if (window.scrollY > 0) {
+    //       navbar.classList.add("sticky");
+    //     } else {
+    //       navbar.classList.remove("sticky");
+    //     }
+    //   });
+      
+      
